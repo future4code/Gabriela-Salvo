7e5829b43679309684cf8fc7c588d709cd265a18
Gabriela <gabrielamdesalvo@gmail.com>
2020-03-10 19:55:50 -0300

    planner ainda inacabado

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,14 +1,58 @@
-
-let segunda =document.querySelector("#segundaFeira")
-let segunda =document.querySelector("#tercaFeira")
-let segunda =document.querySelector("#quartaFeira")
-let segunda =document.querySelector("#quintaFeira")
-let segunda =document.querySelector("#sextaFeira")
-let segunda =document.querySelector("#sabado")
-let segunda =document.querySelector("#domingo")
+//CONSTANTES GLOBAIS
 
 
 
-function  insereTarefa() {
 
+
+function insereTarefa() {
+
+   const input = document.getElementById("input")
+   const novaTarefa =(input).value
+   
+   const selecioneDia = document.getElementById("select")
+   const diaDaSemana = selecioneDia.value
+    
+    const tarefas = document.querySelector("ul")
+    tarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+    
+    if (insereTarefa === " ") {
+       console.log("Quero trabalhar! Digite uma tarefa")
+    }else{
+            switch (diaDaSemana){
+                case "Segunda":
+                    let listaDeTarefasSegunda =document.getElementsByTagName("segundaFeira")
+                    listaDeTarefasSegunda.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+                case "Terca":
+                    let listaDeTarefasTerca =document.getElementById("tercaFeira")
+                  listaDeTarefasTerca.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+                case "Quarta":
+                    let listaDeTarefasQuarta =document.getElementById("quartaFeira")
+                    quarta.innerHTML += "<li>" + novaTarefa + "</li>"
+                    listaDeTarefasQuarta.innerHTML += tarefas+ "<li>" + novaTarefa + "</li>"
+                    break;
+                case "quinta":
+                    quinta.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+                case "sexta":
+                    sexta.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+                case "sabado":
+                    sabado.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+                case "domingo":
+                    domingo.innerHTML += "<li>" + novaTarefa + "</li>"
+                    break;
+        
+        
+        }
+
+    }
 }
+
+
+
+
+
+
