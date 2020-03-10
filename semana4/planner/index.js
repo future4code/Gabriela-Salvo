a6ec366f352cde7769be6d05c9251caeec8d83ec
Gabriela <gabrielamdesalvo@gmail.com>
2020-03-10 16:42:41 -0300

    planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,14 @@
+
+let segunda =document.querySelector("#segundaFeira")
+let segunda =document.querySelector("#tercaFeira")
+let segunda =document.querySelector("#quartaFeira")
+let segunda =document.querySelector("#quintaFeira")
+let segunda =document.querySelector("#sextaFeira")
+let segunda =document.querySelector("#sabado")
+let segunda =document.querySelector("#domingo")
+
+
+
+function  insereTarefa() {
+
+}
