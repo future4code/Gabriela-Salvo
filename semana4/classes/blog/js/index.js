2aaf3cac38453b14126aa32effe712d784f04649
Gabriela <gabrielamdesalvo@gmail.com>
2020-03-11 18:39:48 -0300

    passo 2 classes

--- semana4/classes/blog/js/index.js
+++ semana4/classes/blog/js/index.js
@@ -1,15 +1,32 @@
-function criarPostagem () {
+function criarPostagem() {
     let autor = document.getElementById("nome-autor")
-    let autorDoPost = autor.value
 
-    let titulo= document.getElementById("titulo")
-    let tituloDoPost = titulo.value
+
+    let titulo = document.getElementById("titulo")
+
 
     let postagem = document.getElementById("postagem-blog")
-    let postagemDoBlog = postagem.value
+
+    let buttonPost = document.getElementById("enviar-post")
+
+    let postagemBlog = document.getElementById("postagem")
+
+
+    let objetoBlog = new postBlog(autor.value, titulo.value, postagem.value)
+
+    postagemBlog.innerHTML += "<div>" + objetoBlog.autor + "</div>"
+    postagemBlog.innerHTML += "<div>" + objetoBlog.titulo + "</div>"
+    postagemBlog.innerHTML += "<div>" + objetoBlog.postagem + "</div>"
+}
+
+class postBlog {
+    constructor(autor, titulo, postagem) {
+        this.autor = autor
+        this.titulo = titulo
+        this.postagem = postagem
+    }
 
 }
 
-criarPostagem(){
-    autorDoPost.innerHTML+="<p>"+autor + "</p>"
-}
+
+
