65896bdd0ff9e4530880e53cbd2c4a5f8bd30138
Gabriela <gabrielamdesalvo@gmail.com>
2020-03-13 14:38:05 -0300

    Exercicio 2, escrito

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,27 @@
+/*
+
+ INTERPRETAÇAO DE CODIGO
+
+ 1) Temos uma função com um parâmetro
+dentro dela temos uma constante chamada cotação que será o valor
+convertido para numero do que o usuário digitat
+a função com o return retornará o valor em dólar (que é uma variável)
+ x a cotação do usuário. A constante meuDinheiro receberá o valor 100 do
+ conversorDeMoeda
+ o console irá retornar o valor cotado
+
+
+ 2)A função investeDinheiro recebe dois parâmetros
+ temos uma variável let chamada valorAposInvestimento
+
+ usamos um switch case com o tipo de investimento diferentes que são 4
+ cada um desses investimentos será o valor multiplicado por 1.03 reais
+ se nao for escrito nenhum investiment, aparece um aviso ao usuário
+ a função irá retornar o valor calculado apos as multiplicações
+
+ temos duas novas constantes: uma que recebe  o valor novo do montante que nos dá
+ referência de 150 em Ações e 200 no tesouro direto mas este retornrá falso, pois
+ não existe esse case
+ 
+ 
+*/
