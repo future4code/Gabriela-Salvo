a3be11e29c61dcb77d3c359b426170396459ea3c
Gabriela <gabrielamdesalvo@gmail.com>
2020-03-17 16:55:48 -0300

    introducao ao react

---
+++ semana5/linkedin/astrodev/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+import BigCard from './Components/BigCard/BigCard';
+import SmallCard from './Components/SmallCard/SmallCard';
+import ImageButton from './Components/ImageButton/ImageButon';
+import TituloSecao from './Components/TituloSecao/TituloSecao';
+
+
+function App() {
+  return (
+
+    <div className="titulo">
+      <TituloSecao> </TituloSecao>
+    <div className="Apresentacao"/>
+
+    <BigCard></BigCard>
+    </div>
+      
+   
+  );
+}
+
+
+
+export default App;
---
+++ semana5/linkedin/astrodev/src/Components/SmallCard/SmallCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+import BigCard from './Components/BigCard/BigCard';
+import SmallCard from './Components/SmallCard/SmallCard';
+
+import ImageButton from './Components/ImageButton/ImageButon';
+import TituloSecao from './Components/TituloSecao/TituloSecao';
