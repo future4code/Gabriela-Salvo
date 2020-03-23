99f78a57a1ebf5776049d12288dccc2b6a8e9c69
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-03-23 19:44:13 -0300

    etapa 1

--- semana6/futureforms/src/Components/ExercicioUm/ExercicioUm.js
+++ semana6/futureforms/src/Components/ExercicioUm/ExercicioUm.js
@@ -7,27 +7,76 @@ import './ExercicioUm.css'
 class ExercicioUm extends React.Component {
     constructor (props){
         super(props)
+
+
+        this.state = {
+            // Array inicializado da mesma forma que no Exemplo 5
+             Nome: " ",
+             Idade: " ",
+             Email: " ",
+             Escolaridade:" "
+
+    }
+}
+
+
+
+    onChangeNome =(event)=> {
+        this.setState ({Nome: event.target.value})
+       
+
+
+    }
+    onChangeIdade =(event)=> {
+        this.setState ({Idade: event.target.value})
+       
+
+    }
+    onChangeEmail =(event)=> {
+        this.setState ({Email: event.target.value})
+       
+
+    }
+    onChangeEscolaridade =(event)=> {
+        this.setState ({Escolaridade: event.target.value})
+        
+
+    }
+
+
+    aoClicar =()=>{
+
     }
 
     render(){
         return (
 
             <div className="div-mae">
+                <h1>Informações Pessoais</h1>
 
                 
                 <label for="nome ">Qual o seu nome?</label>
-                <input type="nome" value={this.props.inputNome}>
+                <input type="nome" onChange={this.onChangeNome} value={this.state.Nome}>
                 </input>
 
                 <label for="Idade">Qual a sua idade?</label>
-                <input name="idade" value={this.props.inputIdade}></input>
+                <input name="idade" onChange={this.onChangeIdade} value={this.state.Idade}></input>
 
                 <label for="Email">Insira um email válido: </label>
-                <input name="Email" value={this.props.inputEmail}></input>
+                <input name="Email"  onChange={this.onChangeEmail} value={this.state.Email}></input>
 
 
-                <label for="Escolaridade">Qual sua escoladirade?</label>
-                <input name="Escolaridade" value={this.props.inputEscolaridade}></input>
+                <label for="Escolaridade">Qual sua escolaridade?</label>
+                <select name="Escolaridade" onChange={this.onChangeEscolaridade} value={this.state.Escolaridade}>
+                    <option>Ensino Médio Incompleto</option>
+                    <option>Ensino Médio Completo</option>
+                    <option>Ensino Superior Incompleto</option>
+                    <option>Ensino Superior Completo</option>
+
+                </select>
+
+                <button type="submit"
+                onClick={()=>{this.aoClicar()}} >Enviar</button>
 
 
             </div>
@@ -35,5 +84,6 @@ class ExercicioUm extends React.Component {
     }
     
 }
+
 
 export default ExercicioUm
