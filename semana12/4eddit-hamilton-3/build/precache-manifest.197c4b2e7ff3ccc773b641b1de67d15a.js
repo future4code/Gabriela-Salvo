a75cc06db83c3bb1e7065f488ace886eebe91517
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-05-05 15:12:30 -0300

    adicionando projeto 4eddit

---
+++ semana12/4eddit-hamilton-3/build/precache-manifest.197c4b2e7ff3ccc773b641b1de67d15a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cf714816415b1711d9581450625ed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cc401df78109b6e6f09",
+    "url": "/static/js/2.87f8a89a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.87f8a89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd89f437793fee029de3",
+    "url": "/static/js/main.ba835679.chunk.js"
+  },
+  {
+    "revision": "6ebb3750e6873afc70de",
+    "url": "/static/js/runtime-main.d4c2ee40.js"
+  }
+]);
