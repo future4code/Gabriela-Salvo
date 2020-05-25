9ab0b783c8f2dac494e58948068acc8c9bd4745b
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-05-25 15:23:55 -0300

    refazendo lista de exercicios typescript de segunda feira

---
+++ semana14/semana14-aula1/build/pratice1.js
@@ -0,0 +1,11 @@
+function resultado(a, b) {
+    let operacao = {
+        soma: a + b,
+        mult: a * b,
+        subt: a - b,
+        maior: a > b ? a : b
+    };
+    return operacao;
+}
+console.log(resultado(3, 5));
+//# sourceMappingURL=pratice1.js.map
