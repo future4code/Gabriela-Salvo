73bbbc3c3014089081a89821bb3ff59aa86bb327
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-06-02 19:36:38 -0300

    semana18-aula2

---
+++ semana18/semana18-aula2/src/User.ts
@@ -0,0 +1,29 @@
+export class User {
+  private id: string;
+  private email: string;
+  private name: string;
+  private password: string;
+
+  constructor(id: string, email: string, name: string, password: string) {
+    console.log("Chamando o construtor da classe User");
+    this.id = id;
+    this.email = email;
+    this.name = name;
+    this.password = password;
+  }
+  public introduceYourself(): string {
+    return `ola, ${this.name}, bom dia!`;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
