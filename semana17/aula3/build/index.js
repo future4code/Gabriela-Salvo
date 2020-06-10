b4bdc0f5330018544e9642096c8d32699d032c2d
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-06-10 16:25:14 -0300

    aula 3

---
+++ semana17/aula3/build/index.js
@@ -0,0 +1,47 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const knex_1 = __importDefault(require("knex"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+const connection = knex_1.default({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) || 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
+});
+const app = express_1.default();
+app.use(express_1.default.json());
+const getActorById = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+    `);
+    return console.log(result[0][0]);
+});
+const server = app.listen(process.env.PORT || 3000, () => {
+    if (server) {
+        const address = server.address();
+        console.log(`Server is running in http://localhost:${address.port}`);
+    }
+    else {
+        console.error(`Failure upon starting server.`);
+    }
+});
+getActorById('002');
+//# sourceMappingURL=index.js.map
---
+++ semana17/aula3/src/index.ts
@@ -0,0 +1,40 @@
+import knex from "knex";
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { AddressInfo } from "net";
+import { Request, Response } from "express"
+
+const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) || 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
+});
+
+const app = express();
+
+app.use(express.json());
+
+const getActorById = async (id: string): Promise<any> => {
+    const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+    `)
+    
+    return console.log(result[0][0])
+    
+}
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
+getActorById('002')
